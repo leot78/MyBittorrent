2f8d5a5e8929789e85691fef6fce20a664f298ca
Antoine Léauté <antoine.leaute@epita.fr>
2017-11-14 14:43:41 +0100

    parsing: parsing dictionnary OK, TODO parsing list

--- src/parsing.c
+++ src/parsing.c
@@ -34,7 +34,7 @@ char *parse_string(const char *str, size_t *index)
   char *out = malloc(sizeof(char) * len + 1);
   out = memcpy(out, str + *index, len);
   out[len] = '\0';
-  *index += len;
+  *index += len - 1;
   return out;
 }
 
@@ -47,26 +47,36 @@ char *parse_number(const char *str, size_t *index)
 
   char *res = malloc(sizeof(char) * len + 1);
   res = memcpy(res, str + *index, len);
-  *index += len + 1;
+  *index += len;
   res[len] = '\0';
   return res;
 }
 
 struct dictionary *parse_dict(const char *str, size_t *index)
 {
+  *index += 1;
   struct dictionary *dict = create_dict();
 
   while (str[*index] != 'e')
   {
     char *key = parse_string(str, index);
-    char *value = NULL;
+    void *value = NULL;
 
+    enum type t = CHAR;
+
+    *index += 1;
     if (str[*index] == 'i')
       value = parse_number(str, index);
+    else if (str[*index] == 'd')
+    {
+      t = DICT;
+      value = parse_dict(str, index);
+    }
     else
-      value = parse_string(str, index);
+      value = parse_string(str, index); 
 
-    add_elt(dict, key, value, CHAR);
+    add_elt(dict, key, value, t);
+    *index += 1;
   }
 
   return dict;
@@ -80,8 +90,21 @@ void print_json_dict(struct dictionary *d, int pad)
   {
     struct element *elt = cur->data;
     printf("%*s\"%s\":", pad + 4, "", elt->key);
-    char *value = elt->value;
-    printf("\"%s\"\n", value);
+
+    if (elt->type == CHAR)
+    {
+      char *value = elt->value;
+      printf("\"%s\"", value);
+    }
+    else if (elt->type == DICT)
+    {
+      printf("\n");
+      struct dictionary *dict = elt->value;
+      print_json_dict(dict, pad + 8 + strlen(elt->key));
+    }
+    if (cur->next)
+      printf(",");
+    printf("\n");
   }
   printf("%*s}\n", pad, "");
 
@@ -90,11 +113,18 @@ void print_json_dict(struct dictionary *d, int pad)
 
 int main(int argc, char **argv)
 {
-  argc = argc;
-  argv = argv;
+  if (argc < 2)
+    return 0;
 
-  size_t i = 1;
-  struct dictionary *dict = parse_dict("d11:Hello Worldi1337e4:name7:Antoinee", &i);
+  FILE *file = fopen(argv[1], "r");
+
+  char buf[4096];
+
+  fgets(buf, 4096, file);
+
+  size_t i = 0;
+  struct dictionary *dict = parse_dict(buf, &i);
+
   print_json_dict(dict, 0);
   delete_dict(dict);
 }
