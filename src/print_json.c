b854ec90dad59cf01099d21c9b4d75b43cefe12c
Antoine Léauté <antoine.leaute@epita.fr>
2017-11-16 12:57:10 +0100

    print_json: add files for pretty print

---
+++ src/print_json.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "dictionary.h"
+#include "list/list.h"
+#include "my_bittorrent.h"
+#include "print_json.h"
+
+void print_string(const char *s, size_t size)
+{
+  if (size > 100)
+    printf("<too much data>");
+  else if (!size)
+    printf("%s", s);
+  else
+  {
+    size_t i = 0;
+    while (i < size)
+    {
+      if (s[i] < 0x20 || s[i] > 0x7E)
+        printf("\\u%04X", s[i]);
+      else if (s[i] == '\"')
+        printf("\\\"");
+      else
+        putchar(s[i]);
+      ++i;
+    }
+  }
+}
+
+void print_json_list(struct list *l, int pad)
+{
+  //printf("%*s[", pad, "");
+  printf("[ ");
+  struct node *cur = l->head;
+  for (; cur; cur = cur->next)
+  {
+    struct element *elt = cur->data;
+
+    if (elt->type == CHAR)
+    {
+      printf("\"");
+      print_string(elt->value, elt->size);
+      printf("\"");
+    }
+    else if (elt->type == DICT)
+    {
+      printf("\n");
+      struct dictionary *dict = elt->value;
+      print_json_dict(dict, pad + 4);
+    }
+    else if (elt->type == LIST)
+    {
+      struct list *l = elt->value;
+      print_json_list(l, pad + 8);
+    }
+    if (cur->next)
+      printf(",");
+    printf(" ");
+  }
+  printf("] ");
+}
+
+
+void print_json_dict(struct dictionary *d, int pad)
+{
+  printf("%*s{\n", pad, "");
+  struct node *cur = d->table->head;
+  for (; cur; cur = cur->next)
+  {
+    struct element *elt = cur->data;
+    printf("%*s\"", pad + 4, "");
+    print_string(elt->key, 0);
+    printf("\" : ");
+
+    if (elt->type == CHAR)
+    {
+      printf("\"");
+      print_string(elt->value, elt->size);
+      printf("\"");
+    }
+    else if (elt->type == DICT)
+    {
+      printf("\n");
+      struct dictionary *dict = elt->value;
+      print_json_dict(dict, pad + 8 + strlen(elt->key));
+    }
+    else if (elt->type == LIST)
+    {
+      //printf("\n");
+      struct list *l = elt->value;
+      print_json_list(l, pad + 8 + strlen(elt->key));
+    }
+    if (cur->next)
+      printf(",");
+    printf("\n");
+  }
+  printf("%*s}", pad, "");
+}
+
+void print_json(struct tracker *tr)
+{
+  print_json_dict(tr->dict, 0);
+  printf("\n");
+}
---
+++ src/print_json.h
@@ -0,0 +1,8 @@
+#ifndef PRINT_JSON_H
+# define PRINT_JSON_H
+
+
+void print_json(struct tracker *tr);
+void print_json_dict(struct dictionary *d, int pad);
+
+#endif /* !PRINT_JSON_H */
