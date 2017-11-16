f27f9d7df4889d4ca260272aa4a51720b511f527
Wentzler Quentin <wentzler.quentin@epita.fr>
2017-11-16 23:26:04 +0100

    tracker: create simple peers request and sent it, need testing

--- src/get_tracker.c
+++ src/get_tracker.c
@@ -1,7 +1,9 @@
 #include <curl/curl.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#include "my-bittorrent.h"
+#include "my_bittorrent.h"
 
 size_t get_end_addr(char *urlp)
 {
@@ -12,26 +14,52 @@ size_t get_end_addr(char *urlp)
     if (urlp[i] == ':')
       semic++;
   }
-  return i - 1;
+  return i;
 }
 
-char *get_url(char *urlp, size_t sep_pos)
+/*char *get_url(char *urlp, size_t sep_pos)
 {
   size_t len_urlp = strlen(urlp);
   char *url = malloc(sizeof(char)
-              * (len_urlp - strlen(urlp + sep_pos)));
+              * (len_urlp + 1 - strlen(urlp + sep_pos)));
   if (!url)
     err(1, "Could not allocate string url");
   url = strncpy(url, urlp, sep_pos);
+  url[sep_pos] = '\0';
   return url;
+}*/
 
-char *get_response(char *urlp)
+char *prepare_request(char *port, char *urlp, char *info_hash)
 {
-  size_t sep_pos = get_end_add(urlp);
-  char *url = get_url(urlp, sep_pos);
+  size_t total_size = strlen(urlp) + REQUEST_LEN;
+  char *get_request = malloc(sizeof(char) * total_size);
+  if (!get_request)
+    err(1, "Could not allocate string request");
+  sprintf(get_request,
+          "%s/announce?peer_id=%s&info_hash=%s\n&port=%s&left=0\
+          &downloaded=0&uploaded=0&compact=1",
+          urlp, compute_peer_id(), info_hash, port);
+  return get_request;
+}
+
+char *get_tracker(char *urlp, char *sha1)
+{
+  size_t sep_pos = get_end_addr(urlp);
   CURL *handle = curl_easy_init();
+  char *request = prepare_request(urlp + sep_pos, urlp, sha1);
   char *buff = NULL;
   char errbuff[CURL_ERROR_SIZE];
-  curl_easy_setopt(handle, CURLOPT_URL, url);
-  curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_callback);
-  curl_easy_setopt(handle, CURL_
+  curl_easy_setopt(handle, CURLOPT_URL, request);
+  //curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, write_callback);
+  curl_easy_setopt(handle, CURLOPT_WRITEDATA, &buff);
+  curl_easy_setopt(handle, CURLOPT_ERRORBUFFER, &errbuff);
+  int res = curl_easy_perform(handle);
+  curl_easy_cleanup(handle);
+  curl_global_cleanup();
+  if (res)
+    err(res, errbuff);
+  return buff;
+}
+
+
+
--- src/my_bittorrent.h
+++ src/my_bittorrent.h
@@ -3,8 +3,11 @@
 
 #define PEER_ID_PREFIX "-MB2020-"
 #define PEER_ID_POSTLEN 12
+#define PEER_ID_LEN 20
 #define PEER_ID_MINVALUE 100000000000
 #define PEER_ID_MAXVALUE 999999999999
+
+#define REQUEST_LEN 124
 
 #include <err.h>
 #include <stddef.h>
