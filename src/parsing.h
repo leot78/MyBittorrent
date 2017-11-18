1feeafc3e8728a507a46fc44cd2bdb29a34287d2
Antoine Léauté <antoine.leaute@epita.fr>
2017-11-18 01:03:17 +0100

    parsing: change the all implemenation (FILE -> char *)

--- src/parsing.h
+++ src/parsing.h
@@ -1,8 +1,9 @@
 #ifndef PARSING_H
 # define PARSING_H
 
-struct dictionary *parse_dict(FILE *file);
-struct list *parse_list(FILE *file);
+struct dictionary *parse_dict(char *str, size_t *index);
+struct list *parse_list(char *str, size_t *index);
 struct tracker *parse_file(const char *path);
+struct tracker *parse_content(char *content);
 
 #endif /* PARSING_H */
