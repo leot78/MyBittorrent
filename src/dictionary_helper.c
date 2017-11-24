543f705ee83d6c7c9c0bd9d440846bdf70af434f
Wentzler Quentin <wentzler.quentin@epita.fr>
2017-11-24 19:26:29 +0100

    dictionary_helper: added funct to compute file len

---
+++ src/dictionary_helper.c
@@ -0,0 +1,31 @@
+#include "my_bittorent.h"
+
+size_t get_len_from_files(struct dictionary *dict)
+{
+  char *str_len = get_value(dict, "length", NULL);
+  if (str_len)
+    return atoi(str_len);
+  struct list *file_list = get_value(dict, "files", NULL);
+  if (!file_list)
+    err(1, "cannot find files list");
+
+  size_t len = 0;
+
+  for (struct node *cur = file_list->head; cur; cur = cur->next)
+  {
+    struct element *tmp = cur->data;
+    struct dictionary *tmp_dict = tmp->value;
+    len += atoi(get_value(tmp_dict, "length", NULL));
+  }
+
+  return len;
+}
+
+size_t get_nb_piece(struct dictionary *dict)
+{
+  size_t len = get_len_from_files(dict);
+
+  size_t piece_len = atoi(get_value(dict, "piece length", NULL));
+
+  return len / piece_len;
+}
