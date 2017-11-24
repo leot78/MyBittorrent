20d35ea2ab73b7e6729bd0cf01bd60078d0ec61b
Wentzler Quentin <wentzler.quentin@epita.fr>
2017-11-24 18:40:41 +0100

    message_handler: leeching almost rdy for compil

--- src/message_handler.c
+++ src/message_handler.c
@@ -48,11 +48,26 @@ void piece_case(struct peer *peer, struct raw_mess raw)
   if(g_client.requested != raw.elt)
     return;
   void *tmp = g_client.piece + raw.begin;
-  tmp = memcpy(g_client.piece + raw.begin, raw.elt_3, raw.len - 8);
-  //Je suis débile casting void* essayer &machin
+  tmp = memcpy(g_client.piece + raw.begin, &raw.elt_3, raw.len - 8);
+  //A vérifier.
+  g_client.requested = 0;
 }
 
-char *message_handler(char *message, size_t len, struct peer *peer)
+void make_request(struct peer *peer, struct raw_mess raw
+                  struct list *peer_list)
+{
+  if (g_client.requested)
+    return;
+  unsigned i = 0;
+  for(; i < peer_list->size; i++)
+  {
+    if (peer_list->have[i] && !g_client.have[i] && !peer->client_choked
+        && peer->client_interested)
+    {
+      0
+
+char *message_handler(char *message/*, size_t len*/, struct peer *peer,
+                      struct list *peer_list)
 {
   void *tmp = message;
   struct raw_mess *rm = tmp;
@@ -63,23 +78,35 @@ char *message_handler(char *message, size_t len, struct peer *peer)
   switch(rm->id)
   {
     case 0:
+      peer->client_choked = 1;
+      break;
       //choke
     case 1:
+      peer->client_choked = 0;
+      break;
       //unchoke
     case 2:
+      peer->peer_interested = 1;
+      break;
       //interested
     case 3:
+      peer->peer_interested = 0;
+      break;
       //not interested
     case 4:
+      have_case(peer, rm->elt_1);
+      break;
       //have
     case 5:
+      bitfield_case(peer, rm->elt_1, rm->len);
+      break;
       //bitfield
     case 6:
+      break;
       //request
     case 7:
+      piece_case(peer, rm);
       //piece
-    case 8:
-      //cancel
     default:
       //
   }
