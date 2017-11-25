b31dd1f3e5329854ffa913e96dd3a7ee71c1e0ef
Wentzler Quentin <wentzler.quentin@epita.fr>
2017-11-25 23:53:57 +0100

    coding_style: message_handler.c

--- src/message_handler.c
+++ src/message_handler.c
@@ -9,25 +9,9 @@
 
 struct client g_client;
 
-/*
-{
-  peer->client_choked = 1;
-}
-
-void unchoke_case(struct peer *peer)
-{
-  peer->client_choked = 0;
-}
-
-void interested_case(struct peer *peer)
-{
-  peer->peer_interested = 1;
-}
-
-void not_interested_case(struct peer *peer)
-{
-  peer->peer_interested = 0;
-}
+/**
+** Handle reception of bitfield message.
+** Set the have array according to bitfield.
 */
 void bitfield_case(struct peer *peer, char *bitfield, size_t len)
 {
@@ -37,7 +21,6 @@ void bitfield_case(struct peer *peer, char *bitfield, size_t len)
   {
     for (int i = 7; i >= 0; --i)
     {
-      //peer->have[i] = bitfield[i];
       peer->have[cpt] = (bitfield[j] & (1 << i)) >> i;
       if (peer->have[cpt] && !g_client.have[cpt] && !peer->client_interested)
       {
@@ -51,6 +34,10 @@ void bitfield_case(struct peer *peer, char *bitfield, size_t len)
   }
 }
 
+/**
+** Handle reception of have message.
+** Set have and send INTERESTED if needed.
+*/
 void have_case(struct peer *peer, uint32_t id)
 {
   peer->have[id] = 1;
@@ -61,6 +48,9 @@ void have_case(struct peer *peer, uint32_t id)
   }
 }
 
+/**
+** Check if have_list is full.
+*/
 void check_have(void)
 {
   unsigned i = 0;
@@ -69,24 +59,25 @@ void check_have(void)
   g_client.finish = (i == g_client.number_piece);
 }
 
+/**
+** Handle piece message.
+** Append block to buff, if full piece write to file and call for next
+*/
 void piece_case(struct raw_mess *raw)
 {
   raw->elt_1 = ntohl(raw->elt_1);
   raw->elt_2 = ntohl(raw->elt_2);
-  //raw->elt_3 = ntohl(raw->elt_3);
   if(!g_client.requested)
     return;
   char *tmp = g_client.piece + raw->elt_2;
   tmp = memcpy(tmp, &raw->elt_3, raw->len - 9);
 
-  //A vérifier.
   g_client.piece_len += raw->len - 9;
   g_client.requested = 0;
   size_t piece_len = g_client.piece_max_len;
   if (raw->elt_1 == g_client.number_piece - 1)
   {
-    struct dictionary *dict = get_value(g_client.tracker->dict, "info", 
-                                              NULL);
+    struct dictionary *dict = get_value(g_client.tracker->dict, "info", NULL);
     piece_len = get_len_from_files(dict) % g_client.piece_max_len;
   }
   if (g_client.piece_len == piece_len)
@@ -95,7 +86,6 @@ void piece_case(struct raw_mess *raw)
     {
       write_piece(raw->elt_1);
       g_client.have[raw->elt_1] = 1;
-      //propagate have
       check_have();
     }
     g_client.request_id = -1;
@@ -103,6 +93,9 @@ void piece_case(struct raw_mess *raw)
   }
 }
 
+/**
+** Find peer with with given piece.
+*/
 void verify_have(size_t i, struct peer *peer)
 {
   size_t piece_len = g_client.piece_max_len;
@@ -120,6 +113,9 @@ void verify_have(size_t i, struct peer *peer)
   return;
 }
 
+/**
+** Find peer with any piece not already downloaded.
+*/
 void find_peer(struct node *cur)
 {
   for (; cur; cur = cur->next)
@@ -138,6 +134,9 @@ void find_peer(struct node *cur)
   }
 }
 
+/**
+** Find a peer open to request with a piece not downloaded.
+*/
 void make_request(struct list *peer_list)
 {
   if (g_client.requested)
@@ -162,6 +161,9 @@ void make_request(struct list *peer_list)
   }
 }
 
+/**
+** Send handshake to every peer.
+*/
 void make_all_handshake(struct list *peer_list)
 {
   struct node *cur = peer_list->head;
@@ -172,7 +174,10 @@ void make_all_handshake(struct list *peer_list)
   }
 }
 
-void message_handler(char *message/*, size_t len*/, struct peer *peer,
+/**
+** Handle reception and answer from peers.
+*/
+void message_handler(char *message, struct peer *peer,
                       struct list *peer_list)
 {
   if (message[0] == 0x13)
@@ -185,41 +190,19 @@ void message_handler(char *message/*, size_t len*/, struct peer *peer,
   void *tmp = message;
   struct raw_mess *rm = tmp;
   rm->len = ntohl(rm->len);
-  switch(rm->id)
-  {
-    case 0:
-      peer->client_choked = 1;
-      break;
-      //choke
-    case 1:
-      peer->client_choked = 0;
-      break;
-      //unchoke
-    case 2:
-      peer->peer_interested = 1;
-      break;
-      //interested
-    case 3:
-      peer->peer_interested = 0;
-      break;
-      //not interested
-    case 4:
-      have_case(peer, ntohl(rm->elt_1));
-      break;
-      //have
-    case 5:
-      bitfield_case(peer, message + 5, rm->len);
-      break;
-      //bitfield
-    case 6:
-      break;
-      //request
-    case 7:
-      piece_case(rm);
-      break;
-      //piece
-    default:
-      return;
-  }
+  if (rm->id == 0)
+    peer->client_choked = 1;
+  else if (rm->id == 1)
+    peer->client_choked = 0;
+  else if (rm->id == 2)
+    peer->peer_interested = 1;
+  else if (rm->id == 3)
+    peer->peer_interested = 1;
+  else if (rm->id == 4)
+    have_case(peer, ntohl(rm->elt_1));
+  else if (rm->id == 5)
+    bitfield_case(peer, message + 5, rm->len);
+  else if (rm->id == 7)
+    piece_case(rm);
   make_request(peer_list);
 }
